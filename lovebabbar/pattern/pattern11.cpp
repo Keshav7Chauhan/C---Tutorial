39f9cb988d65df05666179d58804128fa8f53711
Keshav7Chauhan <141233286+Keshav7Chauhan@users.noreply.github.com>
2024-04-01 21:45:03 +0530

    change somthing

--- lovebabbar/pattern/pattern11.cpp
+++ lovebabbar/pattern/pattern11.cpp
@@ -6,30 +6,6 @@ C C C C
 D D D D
 */
 
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     int n;
-//     cin >> n;
-
-//     int row = 1;
-//     while (row <= n)
-//     {
-//         int col = 1;
-//         while (col <= n)
-//         {
-//             col = col + 1;
-
-//             char ch = 'A'+row-1;
-//             cout<<ch<<" ";
-//         }
-//         cout << endl;
-//         row=row+1;
-//     }
-//     return 0;
-// }
-
 #include <iostream>
 using namespace std;
 int main()
@@ -45,7 +21,7 @@ int main()
         {
             col = col + 1;
 
-            char ch = 'A'+col-1;
+            char ch = 'A'+row-1;
             cout<<ch<<" ";
         }
         cout << endl;
@@ -53,4 +29,6 @@ int main()
     }
     return 0;
 }
+
+
 
