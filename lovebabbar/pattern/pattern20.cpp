9f8fe94130b8903d65261212c2982fbc6db5a0f2
Keshav7Chauhan <141233286+Keshav7Chauhan@users.noreply.github.com>
2024-04-04 22:50:52 +0530

    change code

--- lovebabbar/pattern/pattern20.cpp
+++ lovebabbar/pattern/pattern20.cpp
@@ -1,6 +1,46 @@
-#include<iostream>
+/*
+output:-
+        1
+      1 2 1
+    1 2 3 2 1
+  1 2 3 4 3 2 1
+*/
+
+#include <iostream>
 using namespace std;
-int main(){
-    
+int main()
+{
+    int n;
+    cin >> n;
+
+    int row = 1;
+    while (row <= n)
+    {
+        // print kro space(1st triangle)
+        int space = n - row;
+        while (space)
+        {
+            cout << " "<<" ";
+            space = space - 1;
+        }
+
+        // print 2nd triangle
+        int j = 1;
+        while (j <= row)
+        {
+            cout << j <<" ";
+            j = j + 1;
+        }
+
+int start =row-1;
+while(start){
+    cout<<start <<" ";
+    start = start-1;
+}
+
+        cout << endl;
+        row = row + 1;
+    }
+
     return 0;
 }
